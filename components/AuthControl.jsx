f0b205b8df47632be75341a1f0fcf7965550b86c
AnnaSolovykh <ms.teachy.solo@gmail.com>
2024-03-14 19:25:33 -0400

    update AuthControl with session detailes, redirection after logging and add AuthControl to NavBar

--- components/AuthControl.jsx
+++ components/AuthControl.jsx
@@ -1,22 +1,31 @@
+'use client'
+
+import { useSession } from "next-auth/react"
+import { useRouter } from 'next/router';
 import useAuthUser from '../store/useAuthUser';
 
 const AuthControl = () => {
   const { user, login, logout } = useAuthUser();
+  const { data: session } = useSession();
+  console.log({ data: session } )
+  const router = useRouter();
 
   const handleLogin = () => {
     const userData = { id: 1, username: 'exampleUser' };
     login(userData);
+    router.push('/market'); 
     console.log("log in")
   };
 
   const handleLogout = () => {
     logout();
+    router.push('/signin'); 
     console.log("log out")
   };
 
   return (
     <div>
-      {user ? (
+      {user && session ? (
         <>
           <p>Welcome, {user.name}!</p>
           <button onClick={handleLogout}>Logout</button>
